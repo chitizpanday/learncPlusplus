1663a02d584cfb2f939d809422ad583bf816ba2b
chitiz panday <pandchit@isu.edu>
2023-03-18 15:01:29 -0600

    push

---
+++ panday.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+// int main (){
+//    int a = 3, b=5;
+//    std::cout<< "hello choor \n" << a+b <<"\n muji paat";
+//    return 0;
+// }  
+//==================================================================
+// int glo=9;
+
+// void sum (){
+// int a=2, b=3;
+// std::cout<<a+b;
+// }
+
+// int main (){
+// int glo=5;
+// bool yes=true;
+// std::cout<< glo  <<  yes;
+// sum();
+//    return 0;
+// }
+//================================================================
+//Reference variable
+// int main()
+// {
+// int a=7;
+// int & b = a;        //variable b starts pointing a
+// std::cout<<a <<endl;
+// std::cout<<b;
+//    return 0;
+// }
+//=================================================================
+// TYPE CASTING
+// int main()
+// {
+//    int a = 45;
+//    float b= 45.87;
+//    cout<<(float)a<<endl;
+//    cout<<int (b);
+//    return 0;
+// }
+//=================================================================
+// int main()
+// {
+//    int a,b,c;
+//    c=(a+b);
+//    cout<< "give me the first integer";
+//    cin>> a;
+//    cout<< "give me the second integer";
+//    cin>>b;
+//    cout<< "the sum is" <<c;
+
+//    return 0;
+// }
---
+++ tut1.cpp
@@ -0,0 +1,25 @@
+// # include <iostream>
+// using namespace std;
+// int main()
+// {
+//    int num1, num2;
+//    cout<<"give me the first number";
+//    cin>>num1;
+
+//   cout<<"give me the second number";
+//   cin>>num2;
+
+//   cout<< "the sum of two numbers is " << num1+num2;
+//    return 0;
+// }
+# include <iostream>
+using namespace std;
+int main()
+{
+   int a = 4, b=5;
+   cout<<"the aa>b is: " <<(a>b) <<endl;
+   cout<<"the a==b is: " <<(a==b) <<endl;
+   cout<<"the a<b is: " <<(a<b) <<endl;
+   cout<<"the a!=b is: " <<(!(a==b));
+   return 0;
+}
